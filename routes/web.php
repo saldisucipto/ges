7cee6d50dbd11ead01d3b6d6ac21dabdb02d5d3a
saldisucipto <saldisucipto@gmail.com>
2021-09-25 14:25:25 +0700

    Component

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('ges-admin', function () {
+        return view('dashboard');
+    })->name('dashboard');;
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
